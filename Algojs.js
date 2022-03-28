b9a6f53d61e50c7f0b575c88b61fd09e7c1620c1
Sen-Yam <96739035+Sen-Yam@users.noreply.github.com>
2022-03-28 23:27:01 +0000

    name change

---
+++ Algojs.js
@@ -0,0 +1,45 @@
+// super-palindrome 
+function PALINDROME (N) {
+    if(N < 10) {
+        return true;
+    }
+    else {       
+        var A;
+        A=N;
+        var S=0;
+        while(A > 0) {
+           S=S*10 + A%10 ;
+           A=Math.floor(A/10);
+        }
+        if(S==N) {
+            return true;
+        }
+        else {
+            return false;
+        }
+
+    }
+}
+
+// verifier est ce que N possede une racine entiere + verifier si cette racine est palindrome
+function SQRT_PALINDROME(N) {
+    if(N % Math.sqrt(N) !=0) {
+        return false ;
+    }
+    else {
+        return PALINDROME(Math.sqrt(N)) ;  // if (PALINDROME(Math.sqrt(N)))
+    }
+}
+
+//  11 140
+for(var i=4 ; i<=140 ; i++) {
+    if(PALINDROME(i) && SQRT_PALINDROME(i)) {
+        console.log(i);
+    }
+}
+
+
+
+
+
+
